71d55930a72bf585f2e1844bb656aea46e18f465
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-05-29 16:35:05 +0200

    js file edited

--- js/index.js
+++ js/index.js
@@ -9,23 +9,34 @@ fetch('./js/data.json')
 
       // Examine the text in the response
       response.json().then(function(data) {
+        const date = document.querySelector('.bookContainer__pageContainer__date');
         const content = document.querySelector('.bookContainer__pageContainer__textContent');
+        const choicesCtn = document.querySelector('.bookContainer__pageContainer__buttonsContainer');
         const choices = document.querySelectorAll('.choices');
         const choice01 = document.querySelector('.choice_01');
         const choice02 = document.querySelector('.choice_02');
         const nextBtn = document.querySelector('.next_button');
         const prevBtn = document.querySelector('.prev_button');
+        const background = document.querySelector('.backgroundContainer');
         let index = 0;
 
         nextBtn.addEventListener('click', (event) => {
           index++
+          date.innerHTML = data.data[index].date;
           content.innerHTML = data.data[index].text;
-          console.log(index);
-          if (index > 4) {
+          background.innerHTML = data.data[index].background;
+          if (index === 3) {
             nextBtn.classList.add('--hidden');
             prevBtn.classList.add('--hidden');
-            choice01.classList.remove('--hidden');
-            choice02.classList.remove('--hidden');
+            choicesCtn.classList.remove('--hidden');
+          } else if (index === 8) {
+            nextBtn.classList.add('--hidden');
+            prevBtn.classList.add('--hidden');
+            choicesCtn.classList.remove('--hidden');
+            content.innerHTML = data.data[index].text;
+            date.innerHTML = data.data[index].date;
+            choice01.innerHTML = data.data[index].choices[0].choice;
+            choice02.innerHTML = data.data[index].choices[1].choice;
           }
         } )
 
@@ -39,9 +50,23 @@ fetch('./js/data.json')
         for (let i = 0; i < choices.length; i++) {
           choices[i].addEventListener("click", function() {
             index = data.data[index].choices[i].next;
+            console.log(data.data[index].choices[i].next);
             content.innerHTML = data.data[index].text;
-            choice01.innerHTML = data.data[index].choices[0].button;
-            choice02.innerHTML = data.data[index].choices[1].button;
+            date.innerHTML = data.data[index].date;
+            choice01.innerHTML = data.data[index].choices[0].choice;
+            choice02.innerHTML = data.data[index].choices[1].choice;
+            console.log(data.data[index].index);
+            if (data.data[index].index === 7 ) {
+              nextBtn.classList.remove('--hidden');
+              prevBtn.classList.remove('--hidden');
+              choicesCtn.classList.add('--hidden');
+            } else if ((data.data[index].index === 9) || (data.data[index].index === 10) || (data.data[index].index === 11) || (data.data[index].index === 12)) {
+              choicesCtn.classList.add('--hidden');
+            } else {
+              nextBtn.classList.add('--hidden');
+              prevBtn.classList.add('--hidden');
+              choicesCtn.classList.remove('--hidden');
+            }
           })
         }
       });
